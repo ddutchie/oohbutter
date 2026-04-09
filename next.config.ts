487868d53c70aef5025d3833148d2376f8511077
Gerard Slee <ddutchie@gmail.com>
2026-04-09 14:40:33 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/oohbutter",
+  assetPrefix: "/oohbutter",
   images: {
     unoptimized: true, // required for static export
   },
